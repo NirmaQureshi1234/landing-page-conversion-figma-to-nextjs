896fd65e6daef2a313a55420a85776ff5f2c98ed
NirmaQureshi1234 <nirmaqureshi786@gmail.com>
2024-11-20 11:00:20 -0800

    landing-page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,31 @@
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
+
+ 
+ 
+ 
+ 
+
+ 
+
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -12,8 +40,20 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        roboto: ['"Roboto"', "sans-serif"], // Add Roboto font
+      },
+      screens: {
+        xs: "475px", // Extra small screens
+        sm: "640px", // Small screens
+        md: "768px", // Medium screens
+        lg: "1024px", // Large screens
+        xl: "1280px", // Extra large screens
+        "2xl": "1536px", // 2X Extra large screens
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
